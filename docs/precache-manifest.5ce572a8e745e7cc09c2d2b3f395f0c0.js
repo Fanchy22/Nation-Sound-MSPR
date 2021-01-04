1d96c17e63f67e713715a9e70f8176d007ba10f7
Fanchy22 <73825990+Fanchy22@users.noreply.github.com>
2021-01-04 13:45:25 +0100

    test
    version de build

---
+++ docs/precache-manifest.5ce572a8e745e7cc09c2d2b3f395f0c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9824a0b0b98a98bde16d",
+    "url": "Nation-Sound-MSPR/css/app.85784a95.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "Nation-Sound-MSPR/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ac73a28fa3aa182c9382ebfda4cfbe50",
+    "url": "Nation-Sound-MSPR/index.html"
+  },
+  {
+    "revision": "fa5f870db94b2769f7d4",
+    "url": "Nation-Sound-MSPR/js/about.7cd1fa5f.js"
+  },
+  {
+    "revision": "9824a0b0b98a98bde16d",
+    "url": "Nation-Sound-MSPR/js/app.1a31acb5.js"
+  },
+  {
+    "revision": "7760bb930764f892a9e4",
+    "url": "Nation-Sound-MSPR/js/chunk-vendors.fbf5ffa6.js"
+  },
+  {
+    "revision": "76e4579cb3ed1af0e6bd88f848d16339",
+    "url": "Nation-Sound-MSPR/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "Nation-Sound-MSPR/robots.txt"
+  }
+]);
