817798e5b6fe0383f4ec0602125c8dda249f8253
Enzo <enzo.bodingermon@ecoles-wis.net>
2021-01-05 15:18:50 +0100

    update: lineup

--- src/views/Lineup.vue
+++ src/views/Lineup.vue
@@ -1,20 +1,27 @@
 <template>
     <div class="lineup">
-        <div>
-            <b-card
-                    overlay
-                    img-src="https://picsum.photos/900/250/?image=3"
-                    img-alt="Card Image"
-                    text-variant="white"
-                    title="Artist Name"
-            >
-                <b-card-text>
-                    Day - Time
-                </b-card-text>
-                <b-card-text>
-                    Stage
-                </b-card-text>
-            </b-card>
-        </div>
+        <h1>Programmation</h1>
+        <b-container>
+            <b-card-group deck id="lineup" v-for="item in items">
+                <b-card :img-src='item.artistThumbnail' text-variant="dark" style="max-width: 23rem; min-height: 10rem">
+                    <b-card-title>{{item.artistName}}</b-card-title>
+                    <b-card-sub-title>{{item.day}} - {{item.time}}</b-card-sub-title>
+                    <b-card-text>{{item.stage}}</b-card-text>
+                </b-card>
+            </b-card-group>
+        </b-container>
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'Lineup',
+        data() {
+            return{
+            items: [
+                {artistThumbnail: 'https://shorturl.at/ktJQR', artistName: 'Orelsan', day: "Vendredi", time: "8PM", stage: "Pepsi Tent"},
+                {artistThumbnail: 'https://shorturl.at/nqzO0', artistName: 'David Ghetto', day: "Samedi", time: "11PM", stage: "Mainstage"}
+            ]}
+        }
+    }
+</script>
