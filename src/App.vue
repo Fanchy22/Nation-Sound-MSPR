ef1132a39b61917b5f76ebd5c1f4ea1286761930
antoine mourat <antoinemourat@gmail.com>
2021-01-05 12:34:33 +0100

    rien à voir

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,6 @@
     <router-view/>
   </div>
 </template>
-=======
-  <template>
-    <div id="app">
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-        <router-link to="/Footer">Footer</router-link>
-      </div>
-      <router-view/>
 
 <script>
 
