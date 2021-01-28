70c118a20d374e3071a7062f18d2f98078616b61
Enzo <enzo.bodingermon@ecoles-wis.net>
2021-01-28 10:11:28 +0100

    update: billetterie

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,8 +2,8 @@
     <slide right>
         <div id="nav" class="row ml-4 mr-4">
             <router-link to="/" class="text-light">Accueil</router-link>
-            <router-link to="/Billeterie" class="text-light">Programmation</router-link>
-            <router-link to="/Lineup" class="text-light">Billeterie</router-link>
+            <router-link to="/Lineup" class="text-light">Programmation</router-link>
+            <router-link to="/Tickets" class="text-light">Billetterie</router-link><br>
             <router-link to="/Carte" class="text-light">Carte</router-link>
             <router-link to="/About" class="text-light">Informations</router-link>
             <select v-model="$i18n.locale" two>
