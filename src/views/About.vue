98a050c4ba0a65863de210ad16c7c8206d9ae42c
Enzo <enzo.bodingermon@ecoles-wis.net>
2021-02-02 22:15:32 +0100

    new page: News.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="about bg-light h-100" style="padding: 5vw">
-        <h1>Informations</h1>
+        <h1>Informations & FAQ</h1>
+        <b-container>
+            <b-row class="infos-left pb-5">
+                <b-img class="pb-2" :src='"https://bit.ly/35erni9"' style="float: left; height: 20vh; width: 100%; object-fit: cover;"></b-img>
+                <div style="float: left">
+                    <div style="text-align: left; max-width: unset!important;">
+                        <h2 class="pb-2 pt-2" style="font-size: 1.5rem">Lorem ipsum</h2>
+                        <p class="text-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                </div>
+            </b-row>
+        </b-container>
 
     </div>
 </template>
---
+++ src/views/News.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="about bg-light h-100" style="padding: 5vw">
+        <h1>Actualité</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'News'
+    }
+</script>
+
+<style>
+
+</style>
