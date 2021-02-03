0e387302ec582256afdaea579ed3c36603db01d5
Enzo <enzo.bodingermon@ecoles-wis.net>
2021-02-03 10:29:34 +0100

    flui Infos & FAQ

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,12 +3,34 @@
         <h1>Informations & FAQ</h1>
         <b-container>
             <b-row class="infos-left pb-5">
-                <b-img class="pb-2" :src='"https://bit.ly/2YR7ZEF"' style="float: left; height: 20vh; width: 100%; object-fit: cover;"></b-img>
+                <b-img class="mb-4" :src='"https://bit.ly/2YR7ZEF"' style="float: left; height: 20vh; width: 100%; object-fit: cover;"></b-img>
                 <div style="float: left">
-                    <div style="text-align: left; max-width: unset!important;">
-                        <h2 class="pb-2 pt-2" style="font-size: 1.5rem">Lorem ipsum</h2>
-                        <p class="text-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <div class="pb-4" style="text-align: left; max-width: unset!important;" v-for="(information) in informations">
+                        <h2 class="pb-2 pt-2" style="font-size: 1.5rem">{{information.title}}</h2>
+                        <p class="text-dark">{{information.text}}</p>
                     </div>
+                </div>
+            </b-row>
+        </b-container>
+        <b-container>
+            <b-row>
+                <b-img class="pb-2" :src='"https://bit.ly/3pJl4LV"' style="float: left; height: 20vh; width: 100%; object-fit: cover;"></b-img>
+                <div class="pb-4 pt-4 mx-auto">
+                    <h2 style="font-size: 1.5rem">FAQ</h2>
+                    <p>Questions les plus souvent posées</p>
+                </div>
+
+                <div class="accordion col-md-12 mb-3" role="tablist" v-for="(faq, index) in faqs">
+                    <b-card no-body class="mb-1">
+                        <b-card-header header-tag="header" class="p-1 dark" role="tab">
+                            <b-button block v-b-toggle="'accordion-'+index" @click.prevent variant="dark" style="font-weight: 600; text-align: left!important" >{{faq.question}}</b-button>
+                        </b-card-header>
+                        <b-collapse :id="'accordion-'+index" accordion="my-accordion" role="tabpanel">
+                            <b-card-body>
+                                <b-card-text class="text-dark float-left pb-4">{{faq.answer}}</b-card-text>
+                            </b-card-body>
+                        </b-collapse>
+                    </b-card>
                 </div>
             </b-row>
         </b-container>
@@ -17,11 +39,30 @@
 
 <script>
     export default {
-        name: 'About'
+        name: 'About',
+        data() {
+            return {
+                informations: [
+                    {title: 'Title 1', text: 'Lorem Ipsum, ipsum Lorem'},
+                    {title: 'Title 2', text: 'Lorem IpsumSinon quanet ta grand mère en slip'},
+                ],
+                faqs: [
+                    {question: 'Q1', answer: 'A1'},
+                    {question: 'Q2', answer: 'A2'},
+                    {question: 'Q3', answer: 'A3'},
+                    {question: 'Q4', answer: 'A4'},
+                ]
+            }
+        }
     }
 </script>
 
 <style>
+
+    .accordion button:hover{
+        background-color: #000000!important;
+        color: white!important;
+    }
 
 </style>
 
